048c6522e48d1dd93aa7bf3656dc341a3f59122e
E226954P <fremont.tom4848@gmail.com>
2025-01-28 15:29:01 +0100

    ProductRepositoryInterface

--- products/src/main/kotlin/iut/nantes/project/products/repository/ProductRepository.kt
+++ products/src/main/kotlin/iut/nantes/project/products/repository/ProductRepository.kt
@@ -1,7 +1,18 @@
 package iut.nantes.project.products.repository
 
+import ProductEntity
 import iut.nantes.project.products.dto.ProductDto
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-interface ProductRepository : JpaRepository<ProductDto, Int> {
+interface ProductRepositoryCustom {
+    fun save(productEntity: ProductEntity)
+    fun findById(id: String): Optional<ProductEntity>
+    fun findAll(): List<ProductEntity>
+    fun existsByName(name: String): Boolean
+    fun delete(product: ProductEntity)
+}
+
+interface ProductJpaRepository : JpaRepository<ProductEntity, UUID> {
+    fun exists(name: String): Boolean
 }
